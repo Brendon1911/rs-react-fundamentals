c4b5c61cb94d5404ee4f1a7242e7090febe095bb
Brendon Richards <brendonrichards93@gmail.com>
2018-09-30 12:06:20 +0000

    Complete part 3

--- app.jsx
+++ app.jsx
@@ -1,41 +1,56 @@
-class Tweet extends React.Component {
-  render () {
+class Person extends React.Component {
+  
+  render() {
+    let drinkText = this.props.age >= 21 ?
+      "Have a drink!" :
+      "You must be 21.";
+
+    let hobbies = this.props.hobbies.map((hobby,i) => <li key={i}>{hobby}</li>) 
+
     return (
-      <div className="tweet">
-        <span>{this.props.name}</span>
-        <span className="username">{this.props.username}</span>
-        <span className="date">{this.props.date}</span>
-        <p>{this.props.message}</p>
+      <div>
+        <p>Learn some information about this person:</p>
+        <ul>
+          <li>Name: {this.props.name.slice(0,6)}</li>
+          <li>Age: {this.props.age}</li>
+          <ul>Hobbies
+            {hobbies}
+          </ul>
+        </ul>
+        <h3>{drinkText}</h3>
       </div>
     );
   }
 }
 
 class App extends React.Component {
-  render () {
+  render() {
     return (
       <div>
-        <Tweet 
-          name="Matt Lane"
-          username="mmmaaatttttt"
-          date={new Date().toDateString()}
-          message="This app will disrupt everything!!"
+        <Person 
+          name="Homer"
+          age={38}
+          hobbies={["bowling","watching tv","drinking beer"]}
         />
-        <Tweet 
-          name="Elie Schoppik"
-          username="eschoppik"
-          date={new Date().toDateString()}
-          message="#Ilovehashtags"
+        <Person
+          name="Marge"
+          age={34}
+          hobbies={["painting","gambling"]}
         />
-        <Tweet 
-          name="Tim Garcia"
-          username="TimGarcia0"
-          date={new Date().toDateString()}
-          message="Follow me on Twitter!"
+        <Person
+          name="Bart"
+          age={10}
+          hobbies={["skateboarding","making prank calls"]}
+        />
+        <Person
+          name="Lisa"
+          age={8}
+          hobbies={["reading","saxophone","eating vegetables"]}
         />
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// lets put this on the DOM
+ReactDOM.render(<App/>, document.getElementById("app"));
